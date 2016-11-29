294d667da6ad8aacb00bba464a6f933db24a98c6
Jon Marshall <jon@workshopit.co.uk>
2016-11-29 14:08:53 +0000

    added setup.py to test a theory

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(name='Zback',
+      version='1.0',
+      description='ZFS backup management',
+      author='Jon Marshall',
+      author_email='jon@workshopit.co.uk',
+      packages=['zback'],
+     )
