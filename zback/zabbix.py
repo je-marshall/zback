a812ea6ec69c462ee90864fd7f36c415ddc73bd3
Jon Marshall <jon@workshopit.co.uk>
2017-01-31 13:32:36 +0000

    nope

--- zback/zabbix.py
+++ zback/zabbix.py
@@ -54,14 +54,11 @@ def send(socket):
         this_job = []
         this_job.append({'{#DSNAME}' : job['args'][0].name})
         this_job.append({'{#DSLOC}' : job['args'][1]})
-
-        if this_job:
-            format_json(this_job)
         
-    #     if this_job:
-    #         format_list.append(this_job)
+        if this_job:
+            format_list.append(this_job)
 
-    # format_json(format_list)
+    format_json(format_list)
 
 def format_json(in_list):
     '''
