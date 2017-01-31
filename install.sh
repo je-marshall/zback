08c0b7923c19eff137d1f699987ff870899556cc
Jon Marshall <jon@workshopit.co.uk>
2017-01-31 11:46:03 +0000

    Tweaked zabbix and install script

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ echo "----------------------------------------------------------------------"
 [[ -d /etc/zabbix/zabbix_agentd.d/ ]] && ln -sv $ZDIR/conf/userparameter_zback.conf /etc/zabbix/zabbix_agentd.d/userparameter_zback.conf
 cp -fv $ZDIR/conf/zback-client.service /lib/systemd/system/zback-client.service
 cp -fv $ZDIR/conf/zback-server.service /lib/systemd/system/zback-server.service
-ln -sv $ZDIR/bin/zback /usr/bin/zback
+ln -sfv $ZDIR/bin/zback /usr/bin/zback
 
 echo -e "\n"
 echo "----------------------------------------------------------------------"
